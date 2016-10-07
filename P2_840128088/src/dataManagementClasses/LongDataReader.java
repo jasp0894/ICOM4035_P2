94f8e46b3e93615f9739f667652e8a839b52258c
J.A. Sanchez Perez <J.A. Sanchez Perez@JASP>
2016-10-07 17:22:23 -0400

    implementing long data reader

---
+++ P2_840128088/src/dataManagementClasses/LongDataReader.java
@@ -0,0 +1,57 @@
+/**
+ * 
+ */
+package dataManagementClasses;
+
+import java.util.Scanner;
+
+import interfaces.DataReader;
+
+/**
+ * @author J.A. Sanchez Perez
+ *
+ */
+public class LongDataReader implements DataReader {
+
+	private static final int LONGSIZE = Long.BYTES;
+	public static final LongDataReader INSTANCE = new LongDataReader();
+	/**
+	 * 
+	 */
+	private LongDataReader() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/* (non-Javadoc)
+	 * @see interfaces.DataReader#readDataFromArrayOfBytes(byte[], int)
+	 */
+	@Override
+	public Long readDataFromArrayOfBytes(byte[] a, int starting) {
+		// TODO Auto-generated method stub
+		long value = 0;
+		int lSB =0;
+		for (int i = 0; i < LONGSIZE; i++) {
+			value = value <<8;
+			lSB = 0x0000ff & a[starting +i];
+			value = value | lSB;
+		}
+		
+		return value;
+	}
+
+	/* (non-Javadoc)
+	 * @see interfaces.DataReader#readDataFromInputScanner(java.util.Scanner)
+	 */
+	@Override
+	public Object readDataFromInputScanner(Scanner input) {
+		// TODO Auto-generated method stub
+		String s = input.nextLine();
+		
+		try{
+			return Long.parseLong(s);
+		}catch(Exception e){
+			return null; //if the next line does not contain a LONG
+		}
+	}
+
+}
