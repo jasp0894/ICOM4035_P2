e7f8807f9d3b73680bcec06d68b49e0c0e326e8c
J.A. Sanchez Perez <J.A. Sanchez Perez@JASP>
2016-10-07 21:44:28 -0400

    finished comment on attributeInSchema

--- P2_840128088/src/dataManagementClasses/AttributeInSchema.java
+++ P2_840128088/src/dataManagementClasses/AttributeInSchema.java
@@ -8,10 +8,26 @@ import java.io.IOException;
 import java.io.RandomAccessFile;
 import java.util.Scanner;
 
+/**
+ * Specifies a new Attribute as per the specification of project's schema concept.
+ * This type of attribute has DataReader and DataWriter associated to it. These readers and writers
+ * will vary depending on the type of the attribute. Also, it includes a dataOffset as it belongs  to 
+ * a schema.  
+ * @author J.A. Sanchez Perez
+ *
+ */
 public class AttributeInSchema extends Attribute {
+
 	private int dataOffset;     // offset of its value in the records of table it is part of
 	private DataReader dataReader; 
 	private DataWriter dataWriter; 
+	
+	/**
+	 * Creates an instance of this with given name, ID, and dataOffset in schema.
+	 * @param name the name of this 
+	 * @param tIndex Id of this attribute
+	 * @param dataOffset the offset in schema
+	 */
 	public AttributeInSchema(String name, int tIndex, int dataOffset) 
 	{ 
 		super(name, tIndex);  
@@ -20,6 +36,13 @@ public class AttributeInSchema extends Attribute {
 		this.dataWriter = DataUtils.getTypeDataWriter(tIndex); 
 	}
 
+	/**
+	 * 
+	 * Creates an instance of this given a file and the data offset in schema
+	 * @param file RandomAcessFile that contains the attribute
+	 * @param dataOffset the data offset in schema
+	 * @throws IOException if an I/O error occurs
+	 */
 	public AttributeInSchema(RandomAccessFile file, int dataOffset) 
 			throws IOException 
 	{ 
@@ -29,6 +52,10 @@ public class AttributeInSchema extends Attribute {
 		this.dataWriter = DataUtils.getTypeDataWriter(super.gettIndex()); 		
 	}
 	
+	/**
+	 * Gets the data offset of this
+	 * @return the data offset
+	 */
 	public int getDataOffset() { 
 		return dataOffset; 
 	}
@@ -39,14 +66,31 @@ public class AttributeInSchema extends Attribute {
 	}
 	**/
 	
+	/**
+	 * Reads data value from an array of byte starting at position "starting".
+	 * @param a the array of bytes
+	 * @param starting initial position
+	 * @return the read value
+	 */
 	public Object readDataValueFromArrayOfBytes(byte[] a, int starting) { 
 		return dataReader.readDataFromArrayOfBytes(a, starting); 
 	}
 	
+	/**
+	 * Reads data value from an input scanner. 
+	 * @param input the input scanner reference
+	 * @return the read value
+	 */
 	public Object readDataValueFromInputScanner(Scanner input) {  
 		return dataReader.readDataFromInputScanner(input); 
 	}
 	
+	/**
+	 * Write data to an array of bytes. The data starts at position "starting".
+	 * @param a the array
+	 * @param starting the initial position
+	 * @param value the value to write
+	 */
 	public void writeDataValueToArrayOfBytes(byte[] a, int starting, Object value) { 
 		dataWriter.writeDataToArrayOfBytes(a, starting, value);
 	}
