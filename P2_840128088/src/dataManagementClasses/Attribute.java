5bd562a50d57cb5876c4e3ea6113b394198ac827
J.A. Sanchez Perez <J.A. Sanchez Perez@JASP>
2016-10-07 21:12:24 -0400

    Finished comments for Attribute

--- P2_840128088/src/dataManagementClasses/Attribute.java
+++ P2_840128088/src/dataManagementClasses/Attribute.java
@@ -5,16 +5,35 @@ import generalUtilities.DataUtils;
 import java.io.IOException;
 import java.io.RandomAccessFile;
 
+/**
+ * 
+ * An instance of this represents an attribute as per the specifications of this project. 
+ * @author J.A. Sanchez Perez
+ *
+ */
 public class Attribute {
 	private String name; 
 	private int tIndex;         // id of the attribute's data type
 	
+	/**
+	 *  Creates an instance of this with the given name and ID. 
+	 * @param name the name of this attribute
+	 * @param tIndex ID (associated to a data type)
+	 */
 	public Attribute(String name, int tIndex) 
 	{ 
 		this.name = name; 
 		this.tIndex = tIndex; 
 	}
 
+	/**
+	 * Creates an instance of Attribute from the specified file. It is assumed that the file pointer location 
+	 * is located where this attribute data starts. The first byte specifies the data type (ID), the second
+	 * specifies the length of this attribute's name and the next bytes contains the actual name of this attribute
+	 * char by char.
+	 * @param file
+	 * @throws IOException
+	 */
 	public Attribute(RandomAccessFile file) throws IOException { 
 		// reads the data to form this attribute from the RAF file; 
 		// beginning reading from the current file pointer location
@@ -30,23 +49,41 @@ public class Attribute {
 	}
 	
 
+	/**
+	 * Gets the name of this Attribute.
+	 * @return the name
+	 */
 	public String getName() {
 		return name;
 	}
 
+	/**
+	 * Gets the ID (associated to a data type) of this attribute.
+	 * @return the ID.
+	 */
 	public int gettIndex() {
 		return tIndex;
 	}
 
+	/**
+	 * Writes this attribute's properties to file.
+	 * @param file Random Acces file
+	 * @throws IOException if an I/O error occurs
+	 */
 	public void writeToFile(RandomAccessFile file) throws IOException { 
 		file.writeByte((byte) tIndex);
 		file.writeByte((byte) name.length()); 
 		file.writeChars(name);
 	}
 		
+	/**
+	 * Gets the size of the data this attribute holds. 
+	 * @return size
+	 */
 	public int getDataSize() { 
 		return DataUtils.getTypeSize(tIndex); 
 	}
+	
 	
 	public String toString() { 
 		String s="(" + this.name + ", ";
